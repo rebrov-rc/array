ec13c8f049a8eef4e5991f4f6f812f40564aa843
Roman <rebrovdev@gmail.com>
2020-03-31 22:15:09 +0300

    22.14

---
+++ js/calc-2.js
@@ -0,0 +1,16 @@
+let result = 0, simbol;
+
+while (true) {
+
+    simbol = +prompt('ввудите сумму!');
+
+    if (simbol === 0) break;
+
+    let operetion = prompt ('операция!');
+
+    switch(operation) {
+        case '+':
+            result += simbol; 
+    }
+
+}
